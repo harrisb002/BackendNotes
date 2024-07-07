40573b0c59f71f9445f99e0875d602a5313ae428
harrisb002 <harrisbe@sonoma.edu>
2024-07-06 20:01:15 -0700

    working on middleware

--- api/src/app.js
+++ api/src/app.js
@@ -6,6 +6,19 @@ const db = new Database('favorites.db')
 const app = express();
 const port = 3000;
 
+// Defining middleware
+// next, is how you invoke the next step in the middleware pipleine
+app.use((req, res, next) => {
+    console.log('hit 1')
+    next(); // Invoke the next step of middleware/function
+})
+
+app.use((req, res, next) => {
+    console.log('hit 2')
+    next(); 
+})
+
+
 app.get("/favorites", (req, res) => {
     let query = 'SELECT * FROM favorites'
     const sort = req.query.sort;
@@ -22,14 +35,22 @@ app.get("/favorites", (req, res) => {
 });
 
 app.get("/favorites/:id", (req, res) => {
-    const id = parseInt(req.params.id);
-    // const favorite = favorites.find((fav) => fav.id === id)
-    // Use SELECT instead
-    const favorite = db.prepare('SELECT * FROM favorites WHERE id = ?').get(id)
-    if (!favorite) {
-        return res.status(404).json({ error: "Favorite not found" })
+    try {
+        const id = parseInt(req.params.id);
+        // const favorite = favorites.find((fav) => fav.id === id)
+        // Use SELECT instead
+        const favorite = db.prepare('SELECT * FROM favorites WHERE id = ?').get(id)
+        if (!favorite) {
+            // Client side error in 400
+            return res.status(404).json({ error: "Favorite not found" })
+        }
+        res.json({ favorite });
+    } catch (error) {
+        console.log(error)
+        // 500 range is a server error
+        res.status(500).json({error: 'Something went wrong, try again later'})
     }
-    res.json({ favorite });
+
 });
 
 app.listen(port, () => {
